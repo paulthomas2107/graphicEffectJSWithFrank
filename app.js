d07b28793b71c99141cfa81cf029232c2f4b18a1
Paul Thomas <paulthomas2107@gmail.com>
2023-12-01 16:07:10 +0000

    ssss

--- app.js
+++ app.js
@@ -10,9 +10,40 @@ class Cell {
     this.y = y;
     this.width = this.effect.cellWidth;
     this.height = this.effect.cellHeight;
+    this.image = document.getElementById('projectImage');
+    this.slideX = 0;
+    this.slideY = 0;
+    this.vx = 0;
+    this.vy = 0;
   }
   draw(context) {
+    context.drawImage(
+      this.image,
+      this.x + this.slideX,
+      this.y + this.slideY,
+      this.width,
+      this.height,
+      this.x,
+      this.y,
+      this.width,
+      this.height
+    );
     context.strokeRect(this.x, this.y, this.width, this.height);
+  }
+  update() {
+    const dx = this.effect.mouse.x - this.x;
+    const dy = this.effect.mouse.y - this.y;
+    const distance = Math.hypot(dx, dy);
+    if (distance < this.effect.mouse.radius) {
+      const force = distance / this.effect.mouse.radius;
+      this.vx = force;
+      this.vy = force;
+    } else {
+      this.vx = 0;
+      this.vy = 0;
+    }
+    this.slideX += this.vx;
+    this.slideX += this.vy;
   }
 }
 
@@ -21,11 +52,23 @@ class Effect {
     this.canvas = canvas;
     this.width = this.canvas.width;
     this.height = this.canvas.height;
-    this.cellWidth = this.width / 35;
-    this.cellHeight = this.height / 55;
+    this.cellWidth = this.width / 30;
+    this.cellHeight = this.height / 45;
     this.imageGrid = [];
     this.createGrid();
-    console.log(this.imageGrid);
+    this.mouse = {
+      x: undefined,
+      y: undefined,
+      radius: 100,
+    };
+    this.canvas.addEventListener('mousemove', (e) => {
+      this.mouse.x = e.offsetX;
+      this.mouse.y = e.offsetY;
+    });
+    this.canvas.addEventListener('mouseleave', (e) => {
+      this.mouse.x = undefined;
+      this.mouse.y = undefined;
+    });
   }
   createGrid() {
     for (let y = 0; y < this.height; y += this.cellHeight) {
@@ -37,11 +80,17 @@ class Effect {
 
   render(context) {
     this.imageGrid.forEach((cell) => {
+      cell.update();
       cell.draw(context);
     });
   }
 }
 
 const effect = new Effect(canvas);
-console.log(effect);
-effect.render(ctx);
+
+function animate() {
+  effect.render(ctx);
+  requestAnimationFrame(animate);
+}
+
+requestAnimationFrame(animate);
